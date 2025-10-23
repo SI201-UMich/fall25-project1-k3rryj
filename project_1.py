7ae7eca563b1f826d2040ab42ac9bb9dc8b67ea5
Kerrington Jackson <kerryjac@umich.edu>
2025-10-23 16:14:55 -0400

    finished loading function for csv file

--- project_1.py
+++ project_1.py
@@ -17,26 +17,27 @@ def load_csv_file(f):
         csv_file = csv.reader(fh)
         headers = next(csv_file)
         info = []
+        #create function to deal with csv numbers
         def try_float(value):
             try:
                 value = float(value)
             except:
                 return 'NA'
         for row in csv_file:
-            print(row)
             new_dict = {}
+            new_row = row[1:] #get rid of row number
         #row format - Species, island, bill length, 
         # bill depth, flipper length, body mass, sex, year
-            new_dict['Species'] = row[1]
-            new_dict['Island'] = row[2]
-            new_dict['Bill Length'] = try_float(row[3])
-            new_dict['Bill Depth'] = try_float(row[4])
-            new_dict['Flipper Length'] = try_float(row[5])
-            new_dict['Body Mass'] = try_float(row[6])
-            new_dict['Sex'] = row[7]
-            new_dict['Year'] = row[8]
+            new_dict['Species'] = new_row[0]
+            new_dict['Island'] = new_row[1]
+            new_dict['Bill Length'] = try_float(new_row[2])
+            new_dict['Bill Depth'] = try_float(new_row[3])
+            new_dict['Flipper Length'] = try_float(new_row[4])
+            new_dict['Body Mass'] = try_float(new_row[5])
+            new_dict['Sex'] = new_row[6]
+            new_dict['Year'] = new_row[7]
             info.append(new_dict)
-        #print(info)
+        print(info)
         return info
 
 load_csv_file('penguins.csv')
